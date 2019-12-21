0ce2faf75748a6aa68000f5435e6c9820b07fda2
Sven Ruppert <sven.ruppert@gmail.com>
2019-12-21 22:59:10 +0100

    changed demo view

---
+++ 03_demo/src/main/java/demo/UIServiceJava.java
@@ -0,0 +1,19 @@
+package demo;
+
+import com.vaadin.flow.server.Constants;
+import org.apache.commons.cli.ParseException;
+import org.rapidpm.vaadin.nano.CoreUIServiceJava;
+
+public class UIServiceJava extends CoreUIServiceJava {
+
+  private UIServiceJava() {
+  }
+
+  public static void main(String[] args) throws ParseException {
+    System.setProperty(Constants.SERVLET_PARAMETER_ENABLE_DEV_SERVER, "false");
+    System.setProperty("vaadin.compatibilityMode", "true");
+    CoreUIServiceJava uiService = new UIServiceJava().executeCLI(args);
+    uiService.startup();
+  }
+
+}
